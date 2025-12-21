1d4ad890fdf67b1fe7751203d6d132ad9d9aef32
Luca <luca.allegri02@gmail.com>
2025-12-21 09:40:44 +0100

    reusing assignment1 code

--- robot_controller/src/ui_node.cpp
+++ robot_controller/src/ui_node.cpp
@@ -1,4 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include <iostream>
 using std::placeholders::_1;
 
@@ -6,8 +8,111 @@ class InputController : public rclcpp::Node{
     public:
         InputController(): Node("input_controller"){ 
 
+            //TIMERS 
+            this->start();
+
+            //PUBLISHERS
+            intermediate_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/intermediate_vel", 10);
+            
+            //SUBSCRIBERS
+            reverse_state_sub_ = this->create_subscription<std_msgs::msg::Int32>("/is_reversing", 10, std::bind(&InputController::reverse_state_callback, this, _1));
+
+            //VARIABLES
+            stop_vel.linear.x = 0.0;
+            stop_vel.angular.z = 0.0;
+            vel_input.linear.x = 0.0;
+            vel_input.angular.z = 0.0;
+            moved_turtle.data=0.0;
+            is_moving = false;
+            is_reversing = false;
         }
-    private:
+        
+        private:
+
+            void reverse_state_callback(const std_msgs::msg::Int32::SharedPtr msg){
+                int i = msg->data;
+                if(i!=0){
+                    is_reversing=true;
+                    input_timer_.reset();
+                }else{
+                    is_reversing=false;
+                    this->start();
+                }
+            }
+
+            void stop_turtles(){
+                intermediate_vel_pub_->publish(stop_vel);
+                id_turtle_managed_pub_->publish(moved_turtle);
+                is_moving = false;
+                stop_timer_.reset();
+                this->start();
+            }
+
+            void start(){
+                if(!input_timer_ && !is_reversing){
+                    input_timer_ = this->create_wall_timer(
+                        std::chrono::milliseconds(1000), 
+                        std::bind(&InputController::input_timer_callback, this));        
+                }
+            }
+
+            void input_timer_callback(){
+                if(is_moving){
+                    return;              //block if is moving
+                }
+                if(is_reversing){        //block if coming back
+                    return;
+                }
+
+                double linear, angular;
+                std::cout<< "Which turtle do you want to move?\n1) Turtle 1\n2) Turtle 2\n:";
+
+                std::cout<< "Linear Velocity:";
+                if (!(std::cin >> linear)) {
+                    std::cout << "Invalid input for Linear Velocity.\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    return;
+                }
+
+                std::cout << "Angular Velocity:";
+                if(!(std::cin >> angular)) {
+                    std::cout << "Invalid input for Angular Velocity.\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    return;
+                }
+
+                vel_input.linear.x = linear;
+                vel_input.angular.z = angular;
+
+                intermediate_vel_pub_->publish(vel_input);
+
+                is_moving=true;
+                input_timer_.reset();
+
+                stop_timer_ = this->create_wall_timer(
+                    std::chrono::milliseconds(1000), 
+                    std::bind(&InputController::stop_turtles, this)
+                );
+
+            } 
+
+            //TIMERSs
+            rclcpp::TimerBase::SharedPtr input_timer_;
+            rclcpp::TimerBase::SharedPtr stop_timer_;
+
+            //PUBLISHERS
+            rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr intermediate_vel_pub_;
+
+            //SUBSCRIBERS
+            rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr reverse_state_sub_;
+
+            //VARIABLES
+            geometry_msgs::msg::Twist vel_input;
+            geometry_msgs::msg::Twist stop_vel;
+            bool is_reversing;
+            bool is_moving;
         
 };
 
